11f968c610a29c3c40619a7013b8fabec98d3c64
KonnorR <konnor0601@gmail.com>
2022-03-07 07:59:38 -0500

    Update Manipulator.java

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -8,15 +8,18 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SparkConstants;
 import frc.robot.Constants.VictorConstants;
 
 public class Manipulator extends SubsystemBase {
   /** Creates a new Intake. */
+public static RelativeEncoder liftEncoder;
 public static CANSparkMax liftSpark = new CANSparkMax(SparkConstants.liftPort, MotorType.kBrushless);
 public static WPI_VictorSPX rightIntake = new WPI_VictorSPX(VictorConstants.rightIntakePort);
 public static WPI_VictorSPX leftIntake = new WPI_VictorSPX(VictorConstants.leftIntakePort);
@@ -34,6 +37,9 @@ public static WPI_VictorSPX leftIntake = new WPI_VictorSPX(VictorConstants.leftI
     liftSpark.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 15);
     liftSpark.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 0);
 
+    liftEncoder = liftSpark.getEncoder();
+
+    SmartDashboard.putNumber("Lift Position", liftEncoder.getPosition());
   }
 
   public void collect(double power) {
